aea493ca09e70946bcd7c75e719a89f17725d93f
Tim <128478437+timcharlier6@users.noreply.github.com>
2024-02-08 09:42:18 +0100

    formatted node_server

--- node_server/server.js
+++ node_server/server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
-
 const logEvents = require('./logEvents');
 const EventEmitter = require('events');
 class Emitter extends EventEmitter {};
@@ -12,20 +11,26 @@ const PORT = process.env.PORT || 3500;
 
 const serveFile = async (filePath, contentType, response) => {
     try {
+
         const rawData = await fsPromises.readFile(
             filePath,
             !contentType.includes('image') ? 'utf8' : ''
         );
+
         const data = contentType === 'application/json'
             ? JSON.parse(rawData) : rawData;
+
         response.writeHead(
             filePath.includes('404.html') ? 404 : 200,
             {'Content-Type' : contentType}
         );
+
         response.end(
             contentType === 'application/json' ? JSON.stringify(data) : data
         );
+
     } catch (err) {
+
         console.error(err);
         myEmitter.emit(`log`, `${err.name}:${err.message}`, 'errLog.txt');
         response.statusCode = 500;
